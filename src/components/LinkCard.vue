fdabedbd5929fe93b6d56a0a4611e474fba08edc
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-10-05 11:00:50 +0200

    basic hover behavior

--- src/components/LinkCard.vue
+++ src/components/LinkCard.vue
@@ -1,16 +1,16 @@
 <template>
     <div v-for="n in test" class="my_card col p-3 text-center">
-        <div class="border rounded-3 h-100 d-flex flex-column justify-content-around">
+        <a class="card_body border rounded-3 h-100 d-flex flex-column justify-content-around text-black">
             <div class="h-50 d-flex justify-content-center">
                 <img class="img-fluid rounded-circle"
                     src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSC4DtHTGprsp7K8u0ZlfSDmIDplvQYH5vniT0I3rpcl6wqBh8b"
                     alt="">
             </div>
             <div>{{ n }}</div>
-        </div>
+        </a>
     </div>
     <div class="my_card col p-3 text-center">
-        <div class="border rounded-3 h-100 d-flex flex-column justify-content-around">
+        <div class="card_body border rounded-3 h-100 d-flex flex-column justify-content-around">
             <div class="h-50 d-flex justify-content-center">
                 <div class="add_btn border rounded-circle d-flex justify-content-center align-items-center fs-1"></div>
             </div>
@@ -23,11 +23,21 @@
 <style>
 .my_card {
     height: 200px;
+}
 
+.my_card .card_body{
+    text-decoration: none;
+}
+
+.my_card .card_body:hover{
+    background-color: gray;
+    transform: scale(1.1);
+    transition: 0.3s ease;
 }
 
 .add_btn {
     height: 100%;
+    max-width: 50%;
     aspect-ratio: 1 / 1;
     position: relative;
 }
